c82fba93693016b5cde682431938de51c2d48925
Paola Garzón <paolagarzon@MacBook-Pro-de-Paola.local>
2018-04-11 11:51:57 -0500

    greet feature

---
+++ src/app/greet/greet.spec.ts
@@ -0,0 +1,7 @@
+import { greet } from './ greet ’;
+
+describe ('greet', ()=>{ // greet suit
+  it ('should include the name in the message ', () => {
+  expect(greet('adsoft ')).toContain('adsoft ');
+  })
+})
